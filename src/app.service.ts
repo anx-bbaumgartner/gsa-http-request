a402496e1a94790d96bcf373cfdde90286f27772
bbaumgartner <bbaumgartner@anexia-it.com>
2021-11-11 16:33:33 +0100

    first commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getInputParameterValues(body: any): any {
-    console.log('File: ', body.file);
+    console.log('File: ', JSON.stringify(body.file));
     console.log('JSON: ', { body });
 
-    let fileArray: any[] = [];
-    fileArray.forEach((file) => {
-      console.log(file.id);
-    });
+    // let fileArray: any[] = [];
+    // fileArray = body.file;
+    // fileArray.forEach((file) => {
+    //   console.log(file.id);
+    // });
     return body;
   }
 }
