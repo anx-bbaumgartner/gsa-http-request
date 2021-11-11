ed38186f9ab5f9dd0feed60a5457723789a19798
bbaumgartner <bbaumgartner@anexia-it.com>
2021-11-11 16:19:44 +0100

    first commit

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,6 +5,11 @@ export class AppService {
   getInputParameterValues(body: any): any {
     console.log('File: ', body.file);
     console.log('JSON: ', { body });
+
+    let fileArray: any[] = [];
+    fileArray.forEach((file) => {
+      console.log(file.id);
+    });
     return body;
   }
 }
