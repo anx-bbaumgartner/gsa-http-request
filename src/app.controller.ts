47dc0d706431edde05276bd6ca1fce0705d3e0ee
bbaumgartner <bbaumgartner@anexia-it.com>
2021-11-11 16:05:55 +0100

    first commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,11 @@
-import { Body, Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Post()
   getInputParameterValues(@Body() body: any): any {
     return this.appService.getInputParameterValues(body);
   }
